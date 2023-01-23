345b3f4779e500cf6f6f619f390e1b8175264dbc
PC-FAMILY\csi22 <david.rodriguez.alonso10@gmail.com>
2023-01-23 17:36:28 +0100

    TiposDatos

---
+++ EjercicioClaseTiposDatos/src/main/webapp/script/TiposDato.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+ 
+ var Object=new alumno('David','Rodriguez Alonso', 18);
+ document.write(Object instanceof alumno);
+ document.write(Alumno instanceof Object);
+ document.write(typeof(alumno));
+ 
