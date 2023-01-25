3bae91f265b34cfe7dd3dc78883a11c0e841e967
Davidrguez04 <122359982+Davidrguez04@users.noreply.github.com>
2023-01-25 10:03:21 +0100

    cAMBIO

--- EjercicioClaseTiposDatos/src/main/webapp/script/TiposDato.js
+++ EjercicioClaseTiposDatos/src/main/webapp/script/TiposDato.js
@@ -1,9 +1,14 @@
 /**
  * 
  */
+function alumno(nombre,apellidos,edad){
+        this.nombre=nombre;
+        this.apellidos= apellidos;
+        this.edad= edad;
+}
  
- var Object=new alumno('David','Rodriguez Alonso', 18);
- document.write(Object instanceof alumno);
- document.write(Alumno instanceof Object);
- document.write(typeof(alumno));
+ var AlumnoUno=new alumno('David','Rodriguez Alonso', 18);
+ document.write(AlumnoUno instanceof object);
+ document.write(AlumnoUno instanceof alumno);
+ document.write(typeof(AlumnoUno));
  
